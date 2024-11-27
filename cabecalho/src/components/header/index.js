1e43944fd0a0e024b53aa34126adeb9c56a9241d
alinisp <alinisperr@gmail.com>
2024-11-27 07:53:38 -0400

    enter

--- cabecalho/src/components/header/index.js
+++ cabecalho/src/components/header/index.js
@@ -9,7 +9,7 @@ import Image from "next/image";
 export default function Header() {
     const [showMenu, setShowMenu] = useState(true)
     return (
-        <header>
+        <header className={styles.li}>
             <button onClick={() => setShowMenu(!showMenu)}>
                 <Image className={styles.rat}
                     src="/menu.png"
