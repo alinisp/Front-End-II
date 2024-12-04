7732c8a17fcd9c92c32dda83d7d8fc47ae5d1b23
alinisp <alinisperr@gmail.com>
2024-12-04 08:24:44 -0400

    enter

--- projeto/src/app/listas/page.js
+++ projeto/src/app/listas/page.js
@@ -1,15 +1,3 @@
-import styles from "./sobre.module.css";
-
-export default function Sobre() {
-    return (
-        <main>
-            <h1>Qual a personalidade dos pinguins de Madagascar?</h1>
-            <p className={styles.paragrafo}>Capitão, Kowalski, Rico e Recruta ainda acham que são uma tropa de elite dentro do zoológico do Central Park. Quando não estão entretendo os humanos, os irmãos pinguins partem para missões nas perigosas ruas de Nova York.</p>
-            <p id={styles.paragrafoUnico}> Meu paragrafo especial</p>
-        </main>   
-    )
-
-}
 export default function Listas() {
     const racas = ['Vira-lata', 'Pit-bull', 'Pug', 'Shitzu', 'Labrador']
 
