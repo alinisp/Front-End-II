b56c5d1430e5ad72c2dbefee7c0a33be2cda027a
alinisp <alinisperr@gmail.com>
2024-12-18 08:22:48 -0400

    enter

--- projeto/src/components/Header/index.js
+++ projeto/src/components/Header/index.js
@@ -16,6 +16,8 @@ export default function Header() {
                         <li><Link href='/contato'>Contato</Link></li>
                         <li><Link href='/listas'>Listas</Link></li>
                         <li><Link href='/props'>Props</Link></li>
+                        <li><Link href='/filter'>filter</Link></li>
+                        <li><Link href='/efeitos'>Efeitos</Link></li>
                     </ul>
                 </nav>
             }
