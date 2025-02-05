fc6a3d54908d8be404cd8c2baf6fd0cff787fa96
alinisp <alinisperr@gmail.com>
2025-02-05 17:19:18 -0400

    05/02/25

--- projeto2/src/app/page.js
+++ projeto2/src/app/page.js
@@ -2,17 +2,9 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const nome = 'Alini Perroni'
   return (
     <div >
-      <h1 className={styles.tit}>Clínica Médica</h1>
-      <p>Clínica psiquiatrica </p>
-      <p>Médica: {nome}</p>
-      <Image 
-        className={styles.imagem}
-        src="" alt="Imagem de pinguins"
-        width={600} 
-        height={400} />
+      <h1 className={styles.tit}>Clínica Médica Plenitude</h1>
     </div>
   );
 }
