b63331e9b55a1f2e6cb852ccad34067ca3fb7005
alinisp <alinisperr@gmail.com>
2025-02-19 06:17:58 -0400

    enter

--- projeto2/src/app/page.js
+++ projeto2/src/app/page.js
@@ -7,6 +7,12 @@ export default function Home() {
   return (
     <div>
       <h1 className={styles.h1}>Bem Vindo a Clínica Plenitude</h1>
+      <div className={styles.divi}>
+      <p className={styles.paragrafo}>Cuidando da sua saúde com excelência e carinho!
+
+          Na Clínica Plenitude, nosso compromisso é com o seu bem-estar. Contamos com uma equipe de profissionais altamente qualificados e uma estrutura moderna para oferecer o melhor atendimento médico.</p>
+      </div>
+      
 
     </div>
   );
